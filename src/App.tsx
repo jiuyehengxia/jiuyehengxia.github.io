5b72b11ac29e40ee9c497003256fe4ea56a3a0e9
lixian <lixian@cestc.cn>
2026-03-04 14:40:05 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <h1
+      style={{
+        textAlign: 'center',
+        fontSize: '24px',
+        marginTop: '20px',
+      }}
+    >
+      welcome
+    </h1>
+  )
+}
+
+export default App
